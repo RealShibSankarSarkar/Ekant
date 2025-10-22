771b32c18d8cf3664cfb0ad2a0059ba7991aa135
Shib Sankar Sarkar <151728380+RealShibSankarSarkar@users.noreply.github.com>
2025-10-22 12:26:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const client_id = "f7c4febcd8194338a16278f8bd431c15"; // তোমার Spotify App Client ID
+const redirect_uri = "https://realshibsankarsarkar.github.io/ekant/callback.html";
+const scopes = "user-read-private user-read-email"; // যেই permission লাগবে
+
+document.getElementById("login-btn").onclick = () => {
+    const auth_url = `https://accounts.spotify.com/authorize?response_type=token&client_id=${client_id}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(redirect_uri)}`;
+    window.location = auth_url;
+};
